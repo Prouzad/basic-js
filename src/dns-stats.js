f2e3bb16729ad64f528d661a103c18860c259a54
Abdulla <94592483+Prouzad@users.noreply.github.com>
2022-04-28 17:43:38 +0500

    init: add five some task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,47 +1,36 @@
-// const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js');
 
-// /**
-//  * Given an array of domains, return the object with the appearances of the DNS.
-//  *
-//  * @param {Array} domains
-//  * @return {Object}
-//  *
-//  * @example
-//  * domains = [
-//  *  'code.yandex.ru',
-//  *  'music.yandex.ru',
-//  *  'yandex.ru'
-//  * ]
-//  *
-//  * The result should be the following:
-//  * {
-//  *   '.ru': 3,
-//  *   '.ru.yandex': 3,
-//  *   '.ru.yandex.code': 1,
-//  *   '.ru.yandex.music': 1,
-//  * }
-//  *
-//  */
-// function getDNSStats(/* domains */) {
-// 	throw new NotImplementedError('Not implemented');
-// 	// remove line with error and write your code here
-// }
-
-// module.exports = {
-// 	getDNSStats,
-// };
-
-let domains = ['code.yandex.ru', 'music.yandex.ru', 'yandex.ru'];
-
-function get(domain) {
+/**
+ * Given an array of domains, return the object with the appearances of the DNS.
+ *
+ * @param {Array} domains
+ * @return {Object}
+ *
+ * @example
+ * domains = [
+ *  'code.yandex.ru',
+ *  'music.yandex.ru',
+ *  'yandex.ru'
+ * ]
+ *
+ * The result should be the following:
+ * {
+ *   '.ru': 3,
+ *   '.ru.yandex': 3,
+ *   '.ru.yandex.code': 1,
+ *   '.ru.yandex.music': 1,
+ * }
+ *
+ */
+function getDNSStats(domains) {
 	let arr2 = [];
 
 	let arr1 = [];
 	let arr3 = [];
 	let count = 0;
 
-	for (let i = 0; i < domain.length; i++) {
-		let arr = domain[i].split('.');
+	for (let i = 0; i < domains.length; i++) {
+		let arr = domains[i].split('.');
 		arr1.push(arr);
 	}
 
@@ -65,25 +54,27 @@ function get(domain) {
 		}
 	}
 	ik.sort();
+	let arr4 = [];
 	let con = 0;
 	for (let i = 0; i < ik.length; i++) {
 		let co = ik.lastIndexOf(ik[i]) + 1 - ik.indexOf(ik[i]);
-
-		console.log(ik.lastIndexOf(ik[i]));
+		i += co - 1;
+		arr4.push(co);
 	}
-	// for (let i = 0; i < ik.length; i++) {
-	// 	for (let k = 0; k < ik.length; k++) {
-	// 		console.log('1   ' + ik[i]);
-	// 		console.log('lk' + ik[k]);
+	let arr8 = [];
+	for (let key of ik) {
+		if (!arr8.includes(key)) {
+			arr8.push(key);
+		}
+	}
+	const obj = {};
 
-	// 		if (ik[i] == ik[k]) {
-	// 			let a = ik.splice(i, 1);
-
-	// 			con += 1;
-	// 		} else {
-	// 			i++;
-	// 		}
-	// 	}
-	// }
+	for (let i = 0; i < arr8.length; i++) {
+		obj[arr8[i]] = arr4[i];
+	}
+	return obj;
 }
-get(domains);
+
+module.exports = {
+	getDNSStats,
+};
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,156 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	str = '' + str;
+	if (options.addition !== undefined) {
+		options.addition = '' + options.addition;
+	}
+	let arr = [];
+	if (options.repeatTimes == undefined) {
+		arr.push(str);
+		if (options.addition != undefined) {
+			arr.push(options.addition);
+		}
+	} else {
+		for (let i = 0; i < options.repeatTimes; i++) {
+			if (
+				options.additionRepeatTimes !== undefined &&
+				options.additionSeparator == undefined &&
+				options.separator == undefined
+			) {
+				arr.push(str);
+				for (let d = 0; d < options.additionRepeatTimes; d++) {
+					arr.push(options.addition);
+					if (options.additionRepeatTimes != d + 1) {
+						arr.push('|');
+					}
+				}
+
+				if (i === 0) {
+					arr.push('+');
+				}
+			} else if (options.separator == undefined) {
+				arr.push(str);
+				if (options.repeatTimes != i + 1) {
+					if (options.addition != undefined) {
+						arr.push(str);
+
+						arr.push(options.addition);
+					} else {
+						arr.push('+');
+					}
+				}
+			} else if (options.addition == undefined) {
+				arr.push(str);
+				if (options.repeatTimes != i + 1) {
+					arr.push(options.separator);
+				}
+			} else if (options.additionRepeatTimes == undefined) {
+				arr.push(str);
+				if (options.repeatTimes != i + 1) {
+					arr.push(options.addition);
+					arr.push(options.separator);
+				}
+			} else {
+				arr.push(str);
+				for (let j = 0; j < options.additionRepeatTimes; j++) {
+					if (j + 1 == options.additionRepeatTimes) {
+						arr.push(options.addition);
+					} else if (
+						j + 1 != options.additionRepeatTimes &&
+						options.additionSeparator == undefined
+					) {
+						arr.push(options.addition + '+');
+					} else {
+						arr.push(options.addition + options.additionSeparator);
+					}
+				}
+
+				if (i + 1 != options.repeatTimes) {
+					arr.push(options.separator);
+				}
+			}
+		}
+	}
+
+	return arr.join('');
 }
 
 module.exports = {
-  repeater
+	repeater,
 };
+
+// function repeater(str, options) {
+// 	str = '' + str;
+// 	if (options.addition !== undefined) {
+// 		options.addition = '' + options.addition;
+// 	}
+// 	let arr = [];
+// 	if (options.repeatTimes == undefined) {
+// 		arr.push(str);
+// 		if (options.addition != undefined) {
+// 			arr.push(options.addition);
+// 		}
+// 	} else {
+// 		for (let i = 0; i < options.repeatTimes; i++) {
+// 			if (options.separator == undefined) {
+// 				arr.push(str);
+// 				if (options.repeatTimes != i + 1) {
+// 					arr.push('+');
+// 				}
+// 			} else if (options.addition == undefined) {
+// 				arr.push(str);
+// 				if (options.repeatTimes != i + 1) {
+// 					arr.push(options.separator);
+// 				}
+// 			} else if (options.additionRepeatTimes == undefined) {
+// 				arr.push(str);
+// 				if (options.repeatTimes != i + 1) {
+// 					arr.push(options.addition);
+// 					arr.push(options.separator);
+// 				}
+// 			} else {
+// 				arr.push(str);
+// 				for (let j = 0; j < options.additionRepeatTimes; j++) {
+// 					if (j + 1 == options.additionRepeatTimes) {
+// 						arr.push(options.addition);
+// 					} else if (
+// 						j + 1 != options.additionRepeatTimes &&
+// 						options.additionSeparator == undefined
+// 					) {
+// 						arr.push(options.addition + '+');
+// 					} else {
+// 						arr.push(options.addition + options.additionSeparator);
+// 					}
+// 				}
+
+// 				if (i + 1 != options.repeatTimes) {
+// 					arr.push(options.separator);
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return arr.join('');
+// }
+
+// console.log(
+// 	repeater(objWithSpecificCoercion, {
+// 		repeatTimes: 2,
+// 		addition: objWithSpecificCoercion,
+// 	})
+// );
